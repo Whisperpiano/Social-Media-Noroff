6e0c0e891e16281c7172a66c677a9b6aa947254b
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-10-28 09:24:19 +0100

    test netlify

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1 className="bg-yellow-300">Noroff Social Media - With Tailwind CSS</h1>
+      <h1 className="bg-yellow-500 text-white font-2xl font-bold">
+        Noroff Social Media - With Tailwind CSS
+      </h1>
     </>
   );
 }
