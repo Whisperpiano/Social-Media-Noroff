9e7117bade72506e37e4b6735406117f9a2c5885
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-10-31 10:49:53 +0100

    style: singlepost

--- src/components/posts/PostCard.tsx
+++ src/components/posts/PostCard.tsx
@@ -1,28 +1,27 @@
+import { Link } from "react-router-dom";
 import Badge from "../ui/Badge";
 import Divider from "../ui/Divider";
 import UserProfile from "../userPanel/UserProfile";
 import PostFooter from "./PostFooter";
 
 export default function PostCard({ image = false }) {
+  const id = Math.floor(Math.random() * 100);
   return (
     <>
       <article className="flex flex-col gap-5 p-5">
         <UserProfile />
-
-        <p className="text-pretty text-sm font-normal text-tertiary-50">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus
-          vestibulum mi vel ullamcorper. Nulla facilisi. Vestibulum eros ante,
-          tincidunt ac tempor in, molestie ac sem. Curabitur pharetra lacus et
-          dui pharetra, id sollicitudin urna maximus. Etiam nunc turpis, egestas
-          venenatis lectus non, venenatis fringilla felis. Etiam et rhoncus
-          sapien. Sed eu dictum justo, non porttitor orci. Duis sit amet
-          tincidunt tellus. Cras tempus nibh eget ligula interdum lacinia.
-        </p>
-        <div className="flex gap-2.5">
-          <Badge text="react" />
-          <Badge text="react" />
-          <Badge text="react" />
-        </div>
+        <Link to={`/post/${id}`}>
+          <p className="text-pretty text-sm font-normal text-tertiary-50">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            faucibus vestibulum mi vel ullamcorper. Nulla facilisi. Vestibulum
+            eros ante, tincidunt ac tempor in, molestie ac sem. Curabitur
+            pharetra lacus et dui pharetra, id sollicitudin urna maximus. Etiam
+            nunc turpis, egestas venenatis lectus non, venenatis fringilla
+            felis. Etiam et rhoncus sapien. Sed eu dictum justo, non porttitor
+            orci. Duis sit amet tincidunt tellus. Cras tempus nibh eget ligula
+            interdum lacinia.
+          </p>
+        </Link>
 
         {image && (
           <img
@@ -31,6 +30,12 @@ export default function PostCard({ image = false }) {
             className="w-full rounded-lg object-cover object-center"
           />
         )}
+
+        <div className="flex gap-2.5">
+          <Badge text="react" />
+          <Badge text="react" />
+          <Badge text="react" />
+        </div>
 
         <PostFooter />
       </article>
